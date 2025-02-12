7d5c2dfde6be2f9b69a2504a608f79a2f07294a8
Deepak Gurjar <deepak.gurjar@heliostechservices.com>
2025-02-12 15:54:40 +0530

    first commit

--- android/app/src/main/java/com/single_device_app/MainActivity.kt
+++ android/app/src/main/java/com/single_device_app/MainActivity.kt
@@ -1,5 +1,9 @@
 package com.single_device_app
 
+import android.content.Context
+import android.os.Bundle
+import android.view.WindowManager
+import android.widget.Toast
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
@@ -19,4 +23,33 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
       DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+
+
+    /**
+     * Called when the activity is first created.
+     * Initialize kiosk mode settings here.
+     */
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Keep screen on
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+
+        // Prevent status bar pull-down
+        window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        // Check if the app is set as Device Owner
+        val devicePolicyManager = getSystemService(Context.DEVICE_POLICY_SERVICE) as android.app.admin.DevicePolicyManager
+        val componentName = android.content.ComponentName(this, DeviceAdminReceiver::class.java)
+
+        if (devicePolicyManager.isDeviceOwnerApp(packageName)) {
+            // Enable lock task mode
+            devicePolicyManager.setLockTaskPackages(componentName, arrayOf(packageName))
+            startLockTask()
+        } else {
+            Toast.makeText(this, "This app must be set as Device Owner", Toast.LENGTH_LONG).show()
+        }
+    }
+
 }
